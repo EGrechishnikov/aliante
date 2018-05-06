7f03aaf9e662be9460f7353d30996f8bee1a8ede
MacBook <ke6a93@gmail.com>
2018-05-06 18:05:50 +0300

    menu update

--- js/common.js
+++ js/common.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     footerInit();
+    popupInit();
+});
+
+function popupInit() {
     var showingDate = JSON.parse(localStorage.getItem('popupShowingDate'));
     if(showingDate === null || new Date(showingDate).getDate() !== new Date().getDate()) {
         localStorage.setItem('popupShowingDate', JSON.stringify(new Date()));
         $('#popup').modal();
     }
-});
+}
 
 $(window).resize(footerInit);
 
