d7869fecd31c3d0487f5b5bda4f8a50bdd4acc0e
MacBook <ke6a93@gmail.com>
2018-05-06 17:22:33 +0300

    catalog step 1

--- js/common.js
+++ js/common.js
@@ -13,8 +13,11 @@ function footerInit() {
     var margin = 8;
     var footerHeight = $('footer').height();
     var totalElementHeight = margin + footerHeight;
-    var height = window.innerHeight - totalElementHeight > document.body.scrollHeight ?
-        window.innerHeight - totalElementHeight + margin :
-        document.body.scrollHeight + margin;
-    $('footer').css('top', height);
+    if (window.innerHeight - totalElementHeight > document.body.scrollHeight) {
+        $('footer').css('top', (window.innerHeight - totalElementHeight + margin));
+    }
+    // var height = window.innerHeight - totalElementHeight > document.body.scrollHeight ?
+    //     window.innerHeight - totalElementHeight + margin :
+    //     document.body.scrollHeight + margin;
+    // $('footer').css('top', height);
 }
--- js/index.js
+++ js/index.js
@@ -9,10 +9,9 @@ function footerInit() {
     var margin = 8;
     var footerHeight = $('footer').height();
     var totalElementHeight = margin + footerHeight;
-    var height = window.innerHeight - totalElementHeight > document.body.scrollHeight ?
-        window.innerHeight - totalElementHeight + margin :
-        document.body.scrollHeight + margin;
-    $('footer').css('top', height);
+    if (window.innerHeight - totalElementHeight > document.body.scrollHeight) {
+        $('footer').css('top', (window.innerHeight - totalElementHeight + margin));
+    }
 }
 
 function showLogo() {
