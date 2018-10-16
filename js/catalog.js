a853f79e1c312f99d9973527bd955b0d9862a5b2
EGrehishnikov <ke6a93@gmail.com>
2018-10-16 17:31:28 +0300

    catalog

--- js/catalog.js
+++ js/catalog.js
@@ -1,48 +1,63 @@
+"use strict";
+
+var CONTENT_SELECTOR = "#content";
+var TYPE_FACTORY = "FACTORY";
+var TYPE_CATALOG = "CATALOG";
 var baseUrl = "https://aliante.outsystemscloud.com/ALIANTE_ADMIN/rest/api/";
+var step = 0;
 
-$(document).ready(init);
+$(document).ready(render);
 
-function init() {
+function render(id) {
+    if(step === 0) {
+        loadFactories();
+    } else if(step === 1) {
+        loadCatalogs(id);
+    }
+}
+
+function loadFactories() {
+    var factories = sessionStorage.getItem(TYPE_FACTORY);
+    if (factories) {
+        drawFactories(JSON.parse(factories));
+    } else {
+        $.get(baseUrl + "factories").then(drawFactories);
+    }
+}
+
+function loadCatalogs(factoryId) {
+    $(CONTENT_SELECTOR).css('opacity', 0);
+    var factory = JSON.parse(sessionStorage.getItem(TYPE_FACTORY))[factoryId];
+    $.get(baseUrl + "factory/" + factoryId + "/catalogs").then(function(response) {
+        drawCatalogs(factory, response);
+    });
+}
+
+function drawFactories(factories) {
+    sessionStorage.setItem(TYPE_FACTORY, JSON.stringify(factories));
+    var result = "";
+    $(factories).each(function(index, element) {
+        result += drawElement(TYPE_FACTORY, element);
+    });
+    $(CONTENT_SELECTOR).html(result);
+    $(CONTENT_SELECTOR).css('opacity', 1);
     $(".logo").click(function(e) {
-        var id = e.currentTarget.id;
-        if (id) {
-            getFactory(id);
-        }
+        step = 1;
+        render(e.currentTarget.id);
     });
 }
 
-function getCountries() {
-    $.get(baseUrl + "countries").then(function(e) {
-        console.log(e);
-    });
+function drawCatalogs(factoryWrapper, catalogs) {
+    var result = "<h1>" + factoryWrapper.factory.Name + "</h1>";
+    $(CONTENT_SELECTOR).html(result);
+    $(CONTENT_SELECTOR).css('opacity', 1);
 }
 
-function getFactories() {
-    $.get(baseUrl + "factories").then(function(e) {
-        var result = "";
-        $(e).each(function(index, element) {
-            result += "<div><p>" + element.Name + "</p></div>"
-        });
-        console.log(result);
-    });
-}
-
-function getCatalogs() {
-    $.get(baseUrl + "catalogs").then(function(e) {
-        console.log(e);
-    });
-}
-
-function getFactory(id) {
-    console.log(id);
-    // $.get(baseUrl + "factory/" + id).then(function(e) {
-    //     console.log(e);
-    //     console.log(element);
-    // });
-}
-
-function getImage(id) {
-    $.get(baseUrl + "attachment/" + id).then(function(e) {
-        console.log(e);
-    });
+function drawElement(type, element) {
+    switch(type) {
+        case TYPE_FACTORY :
+            return "<div id='" + element.factory.Id + "' class=\"logo\"><img src=\"data:image/png;base64," + element.logo + "\" alt=\"" + element.factory.Name + "\ logo\"></div>";
+        case TYPE_CATALOG :
+            return "";
+    }
 }
