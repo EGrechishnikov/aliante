7ba6f23ff3b1441544eb967499f0848e92537b63
MacBook <ke6a93@gmail.com>
2018-10-20 16:15:01 +0300

    catalog

--- js/catalog.js
+++ js/catalog.js
@@ -10,16 +10,18 @@ var backButton = "<span id='back' class=\"glyphicon glyphicon-chevron-left\" onc
 $(document).ready(render);
 
 function render(id) {
-    if(step === 0) {
+    if (step === 0) {
         loadFactories();
-    } else if(step === 1) {
+    } else if (step === 1) {
         loadCatalogs(id);
+    } else if (step === 2) {
+        loadImages(id);
     }
 }
 
 function changeStep(number, id) {
     step = number;
-    if(step === 0) {
+    if (step === 0) {
         $(CONTENT_SELECTOR).css('opacity', 0);
         setTimeout(render, 500);
     } else {
@@ -32,52 +34,81 @@ function loadFactories() {
     if (factories) {
         drawFactories(JSON.parse(factories));
     } else {
-        $.get(baseUrl + "factories").then(drawFactories);
+        $.get(baseUrl + "factories").then(function (response) {
+            sessionStorage.setItem(TYPE_FACTORY, JSON.stringify(response));
+            drawFactories(JSON.parse(response));
+        });
     }
 }
 
 function loadCatalogs(factoryId) {
     $(CONTENT_SELECTOR).css('opacity', 0);
     var factoriesWrapper = JSON.parse(sessionStorage.getItem(TYPE_FACTORY));
-    var factory = factoriesWrapper.filter(function(e) {return e.factory.Id === Number(factoryId)})[0];
-    $.get(baseUrl + "factory/" + factoryId + "/catalogs").then(function(response) {
-        drawCatalogs(factory, response);
-    });
+    var factory = factoriesWrapper.filter(function (e) {
+        return e.factory.Id === Number(factoryId)
+    })[0];
+    if (factoryId === sessionStorage.getItem("currentFactory")) {
+        drawCatalogs(factory, JSON.parse(sessionStorage.getItem("catalogs")));
+    } else {
+        $.get(baseUrl + "factory/" + factoryId + "/catalogs").then(function (response) {
+            sessionStorage.setItem("catalogs", JSON.stringify(response));
+            sessionStorage.setItem("currentFactory", factoryId);
+            drawCatalogs(factory, response);
+        });
+    }
+}
+
+function loadImages(catalogId) {
+    $(CONTENT_SELECTOR).css('opacity', 0);
+    $.get(baseUrl + "catalog/" + catalogId).then(drawCatalog);
+    $.get(baseUrl + "catalog/" + catalogId + "/images").then(drawImages);
 }
 
 function drawFactories(factories) {
-    sessionStorage.setItem(TYPE_FACTORY, JSON.stringify(factories));
     var result = "";
-    $(factories).each(function(index, element) {
+    $(factories).each(function (index, element) {
         result += drawElement(TYPE_FACTORY, element);
     });
     $(CONTENT_SELECTOR).html(result);
     $(CONTENT_SELECTOR).css('opacity', 1);
-    $(".logo").click(function(e) {
+    $(".logo").click(function (e) {
         changeStep(1, e.currentTarget.id);
     });
 }
 
 function drawCatalogs(factoryWrapper, catalogs) {
     var factory = factoryWrapper.factory;
-    setTimeout(function() {
+    setTimeout(function () {
         var result = "<div id='concrete-factory'>" + backButton + "<h1>" + factory.Name + "</h1>";
-        result += "<img src=\"data:image/png;base64," + factoryWrapper.logo + "\" alt=\"" + factory.Name + "\ logo\"><div><h3>" + factory.Description +"</h3>";
-        $(catalogs).each(function(index, element) {
-            console.log(element);
+        var description = factory.Description ? factory.Description : '';
+        result += "<img src=\"data:image/png;base64," + factoryWrapper.logo + "\" alt=\"" + factory.Name + "\ logo\"><div><h3>" + description + "</h3>";
+        $(catalogs).each(function (index, element) {
             result += drawElement(TYPE_CATALOG, element);
         });
         result += "</div></div>";
         $(CONTENT_SELECTOR).html(result);
         $(CONTENT_SELECTOR).css('opacity', 1);
+        $('.catalog').click(function (e) {
+            changeStep(2, e.currentTarget.id);
+        });
     }, 500);
 }
 
+function drawCatalog(catalog) {
+    console.log(catalog);
+    //TODO
+}
+
+function drawImages(images) {
+    console.log(images);
+    //TODO
+}
+
 function drawElement(type, element) {
-    switch(type) {
+    switch (type) {
         case TYPE_FACTORY :
             return "<div id='" + element.factory.Id + "' class=\"logo\"><img src=\"data:image/png;base64," + element.logo + "\" alt=\"" + element.factory.Name + "\ logo\"></div>";
         case TYPE_CATALOG :
-            return "<div class=\"logo\"><img src=\"data:image/png;base64," + element.logo + "\" alt=\"" + element.catalog.Description + "\"></div>";
+            return "<div id='" + element.catalog.Id + "' class=\"catalog\"><img src=\"data:image/png;base64," + element.logo + "\" alt=\"" + element.catalog.Description + "\"></div>";
     }
 }
