725bd58bc75f62449c71109225d8a3f78995f196
MacBook <ke6a93@gmail.com>
2018-10-22 19:36:29 +0300

    catalog

--- js/catalog.js
+++ js/catalog.js
@@ -5,7 +5,8 @@ var TYPE_FACTORY = "FACTORY";
 var TYPE_CATALOG = "CATALOG";
 var baseUrl = "https://aliante.outsystemscloud.com/ALIANTE_ADMIN/rest/api/";
 var step = 0;
-var backButton = "<span id='back' class=\"glyphicon glyphicon-chevron-left\" onclick='changeStep(0);'></span>";
+var currentCatalog = 0;
+var backButton = "<span id='back' class=\"glyphicon glyphicon-chevron-left\" onclick='changeStep(step - 1, currentCatalog);'></span>";
 
 $(document).ready(render);
 
@@ -36,12 +37,14 @@ function loadFactories() {
     } else {
         $.get(baseUrl + "factories").then(function (response) {
             sessionStorage.setItem(TYPE_FACTORY, JSON.stringify(response));
-            drawFactories(JSON.parse(response));
+            console.log(response);
+            drawFactories(response);
         });
     }
 }
 
 function loadCatalogs(factoryId) {
+    currentCatalog = factoryId;
     $(CONTENT_SELECTOR).css('opacity', 0);
     var factoriesWrapper = JSON.parse(sessionStorage.getItem(TYPE_FACTORY));
     var factory = factoriesWrapper.filter(function (e) {
@@ -77,26 +80,34 @@ function drawFactories(factories) {
 }
 
 function drawCatalogs(factoryWrapper, catalogs) {
-    var factory = factoryWrapper.factory;
+    if (factoryWrapper) {
+        var factory = factoryWrapper.factory;
+        setTimeout(function () {
+            var result = "<div id='concrete-factory'>" + backButton + "<h1>" + factory.Name + "</h1>";
+            var description = factory.Description ? factory.Description : '';
+            result += "<img src=\"data:image/png;base64," + factoryWrapper.logo + "\" alt=\"" + factory.Name + "\ logo\"><div><h3>" + description + "</h3>";
+            $(catalogs).each(function (index, element) {
+                result += drawElement(TYPE_CATALOG, element);
+            });
+            result += "</div></div>";
+            $(CONTENT_SELECTOR).html(result);
+            $(CONTENT_SELECTOR).css('opacity', 1);
+            $('.catalog').click(function (e) {
+                changeStep(2, e.currentTarget.id);
+            });
+        }, 500);
+    }
+}
+
+function drawCatalog(wrapper) {
+    var catalog = wrapper.catalog;
     setTimeout(function () {
-        var result = "<div id='concrete-factory'>" + backButton + "<h1>" + factory.Name + "</h1>";
-        var description = factory.Description ? factory.Description : '';
-        result += "<img src=\"data:image/png;base64," + factoryWrapper.logo + "\" alt=\"" + factory.Name + "\ logo\"><div><h3>" + description + "</h3>";
-        $(catalogs).each(function (index, element) {
-            result += drawElement(TYPE_CATALOG, element);
-        });
+        var result = "<div id='concrete-factory'>" + backButton + "<h1>" + catalog.Name + "</h1>";
+        result += "<img style='max-width: 200px;' src=\"data:image/png;base64," + wrapper.logo + "\" alt=\"" + catalog.Name + "\ logo\"><div>";
         result += "</div></div>";
         $(CONTENT_SELECTOR).html(result);
         $(CONTENT_SELECTOR).css('opacity', 1);
-        $('.catalog').click(function (e) {
-            changeStep(2, e.currentTarget.id);
-        });
     }, 500);
-}
-
-function drawCatalog(catalog) {
-    console.log(catalog);
-    //TODO
 }
 
 function drawImages(images) {
