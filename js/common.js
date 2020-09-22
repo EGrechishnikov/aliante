5876bc1ef81aeb77781b564dddc0e0144a123186
EGrehishnikov <ke6a93@gmail.com>
2020-09-22 14:52:29 +0300

    disable popup

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
-window.onload = popupInit;
-
-function popupInit() {
-    var showingDate = JSON.parse(localStorage.getItem('popupShowingDate'));
-    if(showingDate === null || new Date(showingDate).getDate() !== new Date().getDate()) {
-        localStorage.setItem('popupShowingDate', JSON.stringify(new Date()));
-        $('#popup').modal();
-    }
-}
+// window.onload = popupInit;
+//
+// function popupInit() {
+//     var showingDate = JSON.parse(localStorage.getItem('popupShowingDate'));
+//     if(showingDate === null || new Date(showingDate).getDate() !== new Date().getDate()) {
+//         localStorage.setItem('popupShowingDate', JSON.stringify(new Date()));
+//         $('#popup').modal();
+//     }
+// }
