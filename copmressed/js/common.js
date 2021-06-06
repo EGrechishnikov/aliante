11cdaf96e846a28a7937f42a221f3a8f5f1708c9
Evgeniy Grechishnikov <1>
2021-06-06 15:02:43 +0300

    merge changes from hosting

--- copmressed/js/common.js
+++ copmressed/js/common.js
@@ -1,1 +1,1 @@
-function popupInit(){var e=JSON.parse(localStorage.getItem("popupShowingDate"));null!==e&&new Date(e).getDate()===(new Date).getDate()||(localStorage.setItem("popupShowingDate",JSON.stringify(new Date)),$("#popup").modal())}window.onload=popupInit;
+//function popupInit(){var e=JSON.parse(localStorage.getItem("popupShowingDate"));null!==e&&new Date(e).getDate()===(new Date).getDate()||(localStorage.setItem("popupShowingDate",JSON.stringify(new Date)),$("#popup").modal())}window.onload=popupInit;
